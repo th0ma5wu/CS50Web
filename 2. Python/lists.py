77e5050d997cd909c4af41802b2b773056e08e96
Thomas Wu <thomas@exemplo.com>
2023-05-15 20:40:31 -0300

    CS50 files

---
+++ 2. Python/lists.py
@@ -0,0 +1,10 @@
+# Define a list of names
+
+name = ["Harry", "Ron", "Hermione", "Ginny"]
+
+print(name)
+
+name.append("Draco")
+name.sort()
+
+print(name)
---
+++ 2. Python/sets.py
@@ -0,0 +1,14 @@
+#create an empty set
+
+s = set()
+
+#add elements do set
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.remove(2)
+print(s)
+
+print(f"The set has {len(s)} elements.")
